321815eb76849d3bb8672c7746ddab53bdb6d52a
tony140407 <a32715368@gmail.com>
2021-11-23 19:29:18 +0800

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<script setup>
+import { inject } from "vue";
+
+const axios = inject("axios");
+
+const api = "https://jsonplaceholder.typicode.com/todos/1";
+axios.get(api).then((response) => {
+  console.log("success");
+  console.log(response.data);
+});
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,104 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import axios from "axios";
+import Index from "../views/frontend/Index.vue";
+import HomePage from "../views/frontend/HomePage.vue";
+import Shop from "../views/frontend/Shop.vue";
+import Cart from "../views/frontend/Cart.vue";
+
+import SeeMoreProduct from "../views/frontend/SeeMoreProduct.vue";
+import Login from "../views/admin/Login.vue";
+import Dashboard from "../views/admin/Dashboard.vue";
+import Products from "../views/admin/Products.vue";
+import Orders from "../views/admin/Orders.vue";
+import Coupon from "../views/admin/Coupon.vue";
+const routes = [
+  {
+    path: "/",
+    name: Index,
+    component: Index,
+    children: [
+      {
+        path: "",
+        name: "HomePage",
+        component: HomePage,
+      },
+      {
+        path: "shop",
+        name: "Shop",
+        component: Shop,
+      },
+      {
+        path: "shop/:id",
+        name: "SeeMoreProduct",
+        component: SeeMoreProduct,
+      },
+      {
+        path: "cart",
+        name: "Cart",
+        component: Cart,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/admin",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: "",
+        name: "Products",
+        component: Products,
+      },
+      {
+        path: "orders",
+        name: "Orders",
+        component: Orders,
+      },
+      {
+        path: "coupon",
+        name: "Coupon",
+        component: Coupon,
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: {
+      name: "HomePage",
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // 若需要 requiresAuth 則先經過驗證
+  console.log(to);
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_baseUrl}/api/user/check`;
+    axios.post(api).then((res) => {
+      console.log(res.data);
+      if (res.data.success) {
+        next();
+      } else {
+        next({
+          path: "/login",
+        });
+      }
+    });
+    return;
+  }
+
+  next();
+});
+
+export default router;
