eb50850c43e7cca2996297f50471f874b4191946
tony140407 <a32715368@gmail.com>
2021-12-07 10:01:45 +0800

    add: 新增footer頁面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Navbar />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <script setup>
 import Navbar from "./components/layout/navbar.vue";
+import Footer from "./components/layout/footer.vue";
 import { inject, onMounted, ref } from "vue";
 
 // const VueSweetalert2 = inject("VueSweetalert2");
---
+++ src/components/layout/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="footer">
+    <div class="container">
+      <div class="footer_contactUs">
+        <h4 class="h2 footer_contactUs_title mb-5">聯絡我們</h4>
+        <p class="h4 mb-3">
+          <font-awesome-icon class="fa-1x" :icon="['fas', 'phone-alt']" /> 電話: 0987654321
+        </p>
+        <p class="h4 mb-5">
+          <font-awesome-icon class="fa-1x" :icon="['far', 'envelope']" /> 信箱:
+          Wardrobe_example@gmail.com
+        </p>
+        <p>Copyright © 2021 tony140407. All rights reserved.</p>
+      </div>
+    </div>
+  </section>
+</template>
