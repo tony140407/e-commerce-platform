b0333a5ac04fd8ede3770c9cc91593b87bc42736
tony140407 <a32715368@gmail.com>
2021-12-11 20:07:34 +0800

    add: 新增 banner.vue

---
+++ src/components/frontend/homepage/banner.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="banner">
+    <div class="banner_wrapper container">
+      <div class="banner_welcome">
+        <h2 class="banner_welcome_title">
+          Welcome to <br />
+          Wardrobe!
+        </h2>
+        <router-link class="banner_welcome_btn" to="/shop">前往購物</router-link>
+      </div>
+
+      <div class="banner_triangle d-none d-md-block">別再讓你的衣櫥<br />空空如也!</div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
+<script setup></script>
--- src/views/frontend/HomePage.vue
+++ src/views/frontend/HomePage.vue
@@ -1,4 +1,9 @@
 <template>
-  homePage
-  <font-awesome-icon :icon="['far', 'heart']" />
+  <!-- homePage
+  <font-awesome-icon :icon="['far', 'heart']" /> -->
+  <Banner />
 </template>
+
+<script setup>
+import Banner from "@/components/frontend/homepage/banner.vue";
+</script>
