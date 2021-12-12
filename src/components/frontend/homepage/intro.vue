709675b534b25c7a04c940128e5f87ba5115767c
tony140407 <a32715368@gmail.com>
2021-12-12 19:56:20 +0800

    add: 新增 intro.vue

---
+++ src/components/frontend/homepage/intro.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="intro">
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-12 col-md-6 intro_img intro_img_aboutUs"></div>
+        <div class="col-12 col-md-6 intro_text">
+          <h3 class="intro_text_title">關於 Wardrobe</h3>
+          <p class="intro_text_content">
+            Wardrobe 是家在 2021 年新創立的網路服飾平台，
+            致力於提供消費者平價且高品質的衣服，讓您買的開心、穿的舒適。
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 col-md-6 order-md-2 intro_img intro_img_aboutProducts"></div>
+        <div class="col-12 col-md-6 order-md-1 intro_text">
+          <h3 class="intro_text_title">關於包裹</h3>
+          <p class="intro_text_content">
+            面對一個不斷加快節奏的社會， Wardrobe 相信 24
+            小時內到貨必定會成為未來市場的新標準，故提供全台 24 小時到貨服務。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/frontend/HomePage.vue
+++ src/views/frontend/HomePage.vue
@@ -2,8 +2,10 @@
   <!-- homePage
   <font-awesome-icon :icon="['far', 'heart']" /> -->
   <Banner />
+  <Intro />
 </template>
 
 <script setup>
 import Banner from "@/components/frontend/homepage/banner.vue";
+import Intro from "@/components/frontend/homepage/intro.vue";
 </script>
